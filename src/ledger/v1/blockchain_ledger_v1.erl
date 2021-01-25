87dbc83287ee28ce2eb3229c84dee13116a5ddf2
andymck <andrewb.mckenzie@gmail.com>
2021-01-25 15:58:29 +0000

    fix cf usage

--- src/ledger/v1/blockchain_ledger_v1.erl
+++ src/ledger/v1/blockchain_ledger_v1.erl
@@ -3504,9 +3504,10 @@ invoke_commit_hooks(Changes, Filters) ->
                 %% call the end hook on each group now that all incremental updates are applied
               maps:map(
                 fun(CF, HookList) ->
+                        CFAtom = maps:get(CF, FiltersMap),
                         lists:foreach(
                           fun(#hook{hook_end_fun = HookFun}) ->
-                                  HookFun(CF)
+                                  HookFun(CFAtom)
                           end, HookList)
                 end,
                 Filters)
